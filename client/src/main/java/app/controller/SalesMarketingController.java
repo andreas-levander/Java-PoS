ed0c58bd8c4e0ab82909c12e7635526b5e2a3796
Storm <storm.ramsland@protonmail.com>
2022-10-02 13:35:32 +0300

    Added values for the choiceBox

--- client/src/main/java/app/controller/SalesMarketingController.java
+++ client/src/main/java/app/controller/SalesMarketingController.java
@@ -1,8 +1,12 @@
 package app.controller;
 
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.Scene;
+import javafx.scene.control.ChoiceBox;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 import net.rgielen.fxweaver.core.FxmlView;
@@ -19,12 +23,25 @@ public class SalesMarketingController {
     @FXML
     private BorderPane smBorderPane;
 
+    @FXML
+    private ChoiceBox<String> choiceBox;
+
+    @FXML
+    private Button searchButton;
+
     public void initialize() {
         this.stage = new Stage();
         stage.setScene(new Scene(smBorderPane));
         stage.setTitle("Sales/Marketing");
 
+        ObservableList<String> availableChoices = FXCollections.observableArrayList("","MOST SOLD", "LEAST SOLD");
+        choiceBox.setItems(availableChoices);
 
+        searchButton.setOnAction(actionEvent -> getSelectedChoice());
+    }
+    public String getSelectedChoice() {
+        System.out.println(choiceBox.getSelectionModel().getSelectedItem());
+        return choiceBox.getSelectionModel().getSelectedItem();
     }
     public void show() {
         stage.show();
